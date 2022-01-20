1f2683b02d195818dd8facc41e1b94c10ef2bdb5
Esther Hu <hu.1808@buckeyemail.osu.edu>
2022-01-20 16:10:50 -0500

    fixed menu bug and added more menu button functions

--- ARPG - CSE5912/Assets/Scripts/Controllers/States/Gameplay/GameplayState.cs
+++ ARPG - CSE5912/Assets/Scripts/Controllers/States/Gameplay/GameplayState.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameplayState : BaseGameplayState
 {
+    private static bool res = true;
     public override void Enter()
     {
         base.Enter();
         Debug.Log("entered GameplayState");
         gameplayStateController.gameplayUICanvas.enabled = true;
         pauseMenuButton.onClick.AddListener(() => OnPauseMenuClicked());
+
+        resumeGameButton.onClick.AddListener(() => OnResumeGameClicked());
+        exitToMainMenuButton.onClick.AddListener(() => OnExitToMenuClicked());
+        exitGameButton.onClick.AddListener(() => OnExitGameClicked());
     }
 
     public override void Exit()
@@ -17,6 +23,22 @@ public class GameplayState : BaseGameplayState
         base.Exit();
         // Can remove this line to keep gameplay HUD visible while game is paused.
         gameplayStateController.gameplayUICanvas.enabled = false;
+    }
+    void OnResumeGameClicked()
+    {
+        ResumeGame();
+    }
+
+    void OnExitToMenuClicked()
+    {
+        Time.timeScale = 1;
+        res = true;
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    void OnExitGameClicked()
+    {
+        Application.Quit();
     }
 
     void OnPauseMenuClicked()
@@ -28,13 +50,35 @@ public class GameplayState : BaseGameplayState
     {
         if (Input.GetKeyUp(KeyCode.Escape))
         {
-            Debug.Log("Escape Pressed GP State");
-            PauseGame();
+            if (res)
+            {
+                Debug.Log("Escape Pressed GP State");
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
         }
     }
 
     void PauseGame()
     {
-        gameplayStateController.ChangeState<PauseGameState>();
+        gameplayStateController.gameplayUICanvas.enabled = false;
+        Debug.Log("Game paused");
+        Time.timeScale = 0;
+        gameplayStateController.pauseMenuCanvas.enabled = true;
+        res = false;
+        //gameplayStateController.ChangeState<PauseGameState>();
+    }
+    void ResumeGame()
+    {
+        Debug.Log("entered GameplayState");
+        gameplayStateController.gameplayUICanvas.enabled = true;
+       // base.Exit();
+        gameplayStateController.pauseMenuCanvas.enabled = false;
+        Time.timeScale = 1;
+        res = true;
+        // gameplayStateController.ChangeState<GameplayState>();
     }
 }
