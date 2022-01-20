dad615c234a4cc7e394b66720b089f0673cb1032
HankLiu123 <liu.7220@buckeyemail.osu.edu>
2022-01-19 19:18:09 -0500

    update health bar

--- ARPG - CSE5912/Assets/Scripts/HealthBarController.cs
+++ ARPG - CSE5912/Assets/Scripts/HealthBarController.cs
@@ -8,21 +8,35 @@ public class HealthBarController : MonoBehaviour
     public Image healthBar;
     public float maxHealth = 100f;
     public float currHealth;
+
+    float lerpSpd;
     // PlayerController_Script Player;  for updating current player health, not using at this point
 
     private void Start()
     {
         currHealth = maxHealth;
+        
     }
 
     private void Update()
     {
+        if (currHealth > maxHealth) currHealth = maxHealth;
+        lerpSpd = 3f * Time.deltaTime;
         HealthBarFiller();
+        colorChanger();
+
     }
 
+    /* change the health bar with a lerp speed*/
     public void HealthBarFiller()
     {
-        healthBar.fillAmount = currHealth / maxHealth;
+        healthBar.fillAmount = Mathf.Lerp(healthBar.fillAmount, currHealth / maxHealth, lerpSpd);
+    }
+    public void colorChanger()
+    {
+        Color healthC = Color.Lerp(Color.red, Color.green, (currHealth / maxHealth));
+        healthBar.color = healthC;
+        
     }
     public void HitDamage(float damageRate)
     {
